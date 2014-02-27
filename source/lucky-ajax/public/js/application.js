83809fda159f045ea6f220b0f63d7132060e631d
& <pair+mknudsen01.trekkie4life@devbootcamp.com>
2014-02-26 16:58:06 -0800

    Add jQuery to submit an AJAX post to the form's action
    when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery

--- source/lucky-ajax/public/js/application.js
+++ source/lucky-ajax/public/js/application.js
@@ -1,10 +1,25 @@
 $(document).ready(function () {
 
   // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- generate a random number between 1 and 6 using JavaScript
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
+
+
+  $(".roll_button").on("click", function() {
+    event.preventDefault();
+    var roll = (Math.floor(Math.random() * 6) + 1);
+
+    $.ajax({
+      type: "POST",
+      url: "/rolls",
+      data: { value: roll },
+      success: function(response){
+          //change the dice picture
+        $('.die_pic').replaceWith('<img class="die_pic" src="/'+ roll + '.png" title="'+ roll+ '" alt="the roll">');
+      }
+    })
+
+
+  });
 });
