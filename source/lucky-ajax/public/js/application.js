bb47ba16ce0512eeba7d3bba55278ab8b9ab1d1a
Mason O'Neal & Tim <pair+masononeal.tyau@devbootcamp.com>
2014-02-26 16:25:33 -0800

    Get AJAX dice roll without refreshing page
    Block form sublit, rolls a local dice, pass it to server
    If server doesn't get it, server rolls its own dice
    
    the dice roll passed to or locally generated is saved to
    the database, and then passed back to javascript as a
    message.
    
    Javascript changes the page based off what is passed back to it
    from the server.

--- source/lucky-ajax/public/js/application.js
+++ source/lucky-ajax/public/js/application.js
@@ -7,4 +7,58 @@ $(document).ready(function () {
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
+  roll();
+
 });
+
+var postRoll = function(){
+  var rand = Math.floor((Math.random() * 6) + 1);
+  console.log("your roll is: " + rand)
+  $.ajax({
+    type: "POST",
+    url: "/rolls",
+    data: { value: rand }
+  })
+    .success(function( msg ) {
+      console.log("you got your roll from the server");
+      $("#die img").attr({
+        src: "/" + msg + ".png",
+        title: msg
+      })
+    })
+    .fail( function(rand){
+      console.log("you got a local roll");
+      $("#die img").attr({
+        src: "/" + rand + ".png",
+        title: rand
+      });
+    });
+}
+
+var roll = function(){
+  console.log("start roll");
+  //$("input").on("click", function(){ //this will save the click behavior; causes multiple "clicks"
+  $("form").submit( function(event){
+      event.preventDefault();
+      postRoll();
+    }
+  );
+ // });
+  console.log("end roll");
+}
+
+//$("#die img")
+
+// We're going to practice converting a working
+// Sinatra+ActiveRecord
+// app into a
+// Sinatra+ActiveRecord+jQuery+AJAX app.
+
+// Specifically, we're going to take an application that
+// reloads an entire page every time the user submits a form
+// into one that just updates the portion of the DOM that needs
+// to change.
+
+    // <% if @roll %>
+    //   <img src="/<%= @roll.value %>.png" title="<%= @roll.value %>" alt="the roll">
+    // <% end %>
