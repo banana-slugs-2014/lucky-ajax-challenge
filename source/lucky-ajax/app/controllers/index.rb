9b04478522e09acd02157d4c75f3a4d7c6014315
& <pair+chriswoodrich.thefenry@devbootcamp.com>
2014-02-26 18:49:34 -0800

    Release 0-2

--- source/lucky-ajax/app/controllers/index.rb
+++ source/lucky-ajax/app/controllers/index.rb
@@ -4,12 +4,13 @@ end
 
 
 # TODO: convert this route to use AJAX
-post '/rolls' do
+post '/rolls.json' do
   # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
   # See: roll_if_value_is_nil method in the Roll model.
-  value = params[:value] ? params[:value].to_i : nil
+  # value = params[:value] ? params[:value].to_i : nil
 
-  @roll = value ? Roll.create({ value: value }) : Roll.create
+  # @roll = value ? Roll.create({ value: value }) : Roll.create
 
-  erb :index  # HINT: what does this do? what should we do instead?
+ # erb :index  # HINT: what does this do? what should we do instead?
+ # send data back to success function, then change the DOM
 end
--- source/lucky-ajax/public/js/application.js
+++ source/lucky-ajax/public/js/application.js
@@ -1,10 +1,39 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-  // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- generate a random number between 1 and 6 using JavaScript
-  //   4- use jQuery to submit an AJAX post to the form's action
-  //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
+//   // PSEUDO-CODE:
+//   //   1- intercept the form submission event using jQuery
+//   //   2- prevent the default action for that event from happening
+//   //   3- generate a random number between 1 and 6 using JavaScript
+//   //   4- use jQuery to submit an AJAX post to the form's action
+//   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
-});
+// });
+
+
+$(document).ready(function() {
+  DiceWrapper.addListener()
+})
+
+var  DiceWrapper = {
+  addListener: function (){
+    $('form').submit(getResponse)
+  }
+}
+
+var getResponse = function(e){
+    var diceRoll =getDiceRoll()
+    e.preventDefault();
+    $.ajax({
+    type: "POST",
+    url: "/rolls.json",
+    data: diceRoll,
+    success: function(response){
+      $('.changeMe').attr("src", diceRoll + '.png');
+      console.log(diceRoll);
+      }
+    });
+}
+
+var getDiceRoll = function (){
+  return Math.floor((6*Math.random()+1))
+}
