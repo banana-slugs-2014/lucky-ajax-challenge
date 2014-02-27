0ebad46cd8d642f1528ad128e03b1a4c1f6ac225
& <pair+PhilVargas.ireneyiu@devbootcamp.com>
2014-02-26 17:09:15 -0800

    release2 finished. ajaxyified with OO

--- source/lucky-ajax/public/js/application.js
+++ source/lucky-ajax/public/js/application.js
@@ -7,4 +7,45 @@ $(document).ready(function () {
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
+  Die.initialize();
+
 });
+
+var Die = {
+  initialize: function(){
+    $('#form-die').on('submit',function(event) {
+      event.preventDefault();
+      Die.dieAjaxRequest();
+    });
+  },
+
+  rollDie: function() {
+    return Math.floor((Math.random()*6)+1);
+  },
+
+  updateDie: function(rollValue) {
+    $('#die').empty().append(
+      $('<img>').attr({
+        src: rollValue + '.png',
+        title: rollValue,
+        alt: "the roll"
+      })
+    );
+  },
+
+  dieAjaxRequest: function(){
+    $.ajax({
+      type: "POST",
+      url: "/rolls",
+      data: {value: Die.rollDie()},
+      dataType: "json",
+      success: function(response) {
+        Die.updateDie(response.value);
+        console.log("success")
+      },
+      error: function(response){
+        console.log(response)
+      }
+    });
+  }
+}
