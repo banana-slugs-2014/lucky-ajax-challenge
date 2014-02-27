84da72277fd04c4dda804d492a24cd051abe01ba
& <pair+harryw377.hpchess@devbootcamp.com>
2014-02-26 18:37:52 -0800

    Add MVC implementation of lucky-ajax.
    All in one file. Sorry...

--- source/lucky-ajax/public/js/application.js
+++ source/lucky-ajax/public/js/application.js
@@ -1,17 +1,70 @@
 $(document).ready(function () {
 
-  $('form').submit(function(e){
-    e.preventDefault();
-    $.post("/rolls",{}, function(data, textStatus, jqXHR) {
-      $('#die img').attr("src", data + ".png");
-    });
-  });
-
-  // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- generate a random number between 1 and 6 using JavaScript
-  //   4- use jQuery to submit an AJAX post to the form's action
-  //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
+  DiceApp.controller = new DiceApp.Controller(new DiceApp.View(DiceApp.dieViewSelector));
+  new DiceApp.Binder( DiceApp.dieEventSelector, DiceApp.controller ).rollBinder();
 
 });
+
+// Controller - JS and Server
+// View - Updates page (i.e. dice)
+// Model - Server(dice class on server)
+// Binder - Binds the submit event on the form button
+
+// Selectors
+// var dieEventSelector = 'form'
+// var dieViewSelector = '#die img'
+
+// App
+var DiceApp = {
+  dieViewSelector: '#die img',
+  dieEventSelector: 'form'
+}
+
+// Controller
+DiceApp.Controller = function(view) {
+  this.view = view
+}
+
+DiceApp.Controller.prototype = {
+  getRoll: function() {
+    var view = this.view
+    $.ajax({
+      type: 'POST',
+      url: "/rolls",
+      success: function(resp){
+        view.updateImage(resp)
+      }
+    });
+  }
+}
+
+// View
+DiceApp.View = function(){}
+
+DiceApp.View.prototype = {
+  updateImage: function(data){
+    $('#die img').attr("src", data + ".png");
+  },
+}
+
+
+// Binding
+DiceApp.Binder = function(target, controller){
+  this.target = target;
+  this.controller = controller;
+}
+
+DiceApp.Binder.prototype = {
+
+  rollBinder: function(){
+
+    var controller = this.controller
+
+    $(this.target).submit(function(e){
+      e.preventDefault();
+      controller.getRoll();
+    });
+  }
+}
+
+
