b52654c5d394b64b5708608d2106661a27b0582e
& <pair+papapoison.qt-dev@devbootcamp.com>
2014-02-26 19:40:19 -0800

    Couldn't get JSON to work. But the app itself is working

--- source/lucky-ajax/config/environment.rb
+++ source/lucky-ajax/config/environment.rb
@@ -16,9 +16,12 @@ require 'active_record'
 require 'logger'
 
 require 'sinatra'
+require 'sinatra/json'
+require 'json'
 require 'erb'
 
 require 'shotgun'
+
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
--- source/lucky-ajax/public/js/application.js
+++ source/lucky-ajax/public/js/application.js
@@ -1,10 +1,60 @@
+var Die = function(){
+  this.side = 0;
+};
+
+Die.prototype = {
+  roll: function(){
+    this.side = Math.floor( Math.random()*6 + 1 )
+  }
+};
+
+
+var WindowManager = function(){}
+
+WindowManager.prototype = {
+  changeDieSide: function(side){
+    $('img#die-img').remove();
+    $('#die').append('<img src="/'+side+'.png" title="'+side+'" alt="the roll" id="die-img">');
+  }
+};
+
+
+
+var Controller = function(){
+  this.die = new Die();
+  this.windowManager = new WindowManager();
+}
+
+Controller.prototype = {
+  attachRollListener: function(){
+    var self = this;
+    $('#roll').on('click', function(event){
+      event.preventDefault();
+      self.roll();
+    });
+  },
+
+  roll: function(){
+    this.die.roll();
+    this.ajaxRoll(die.side);
+  },
+
+  ajaxRoll: function(roll){
+    var self = this;
+    $.ajax({
+        url: '/rolls',
+        type: "POST",
+        data: { value: roll },
+        success: function(response){
+          self.windowManager.changeDieSide(response);
+        }
+    });
+  }
+}
+
+
+
 $(document).ready(function () {
-
-  // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- generate a random number between 1 and 6 using JavaScript
-  //   4- use jQuery to submit an AJAX post to the form's action
-  //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
-
+  dieController = new Controller();
+  dieController.attachRollListener();
 });
